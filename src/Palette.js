dc06e9316353e2ea9a27e91b8766f64c381f7af8
niansahc <97317555+niansahc@users.noreply.github.com>
2022-01-20 13:52:04 -0500

    Created reactr app and palette.

---
+++ src/Palette.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* Navigation Bar Here */}
+                <div classname='Palette-colors'>
+                    {/*creating our colour boxes */}</div>
+                {/* {Footer Forthcoming} */}
+            </div>
+        )
+    }
+}
